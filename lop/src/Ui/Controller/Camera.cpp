d2029a7f881b11ead9203c0a277049a9abc59b82
Cyprien Plateau--Holleville <48133083+PlathC@users.noreply.github.com>
2023-03-25 15:46:12 +0100

    Feat: Improvements to the camera controller

--- lop/src/Ui/Controller/Camera.cpp
+++ lop/src/Ui/Controller/Camera.cpp
@@ -27,23 +27,34 @@ namespace lop
 
         if (inputs.mouseLeftPressed)
         {
-            const float     strength = handleStrength(1.f);
+            const float     strength = handleStrength(1e-3f);
             const vzt::Vec2 delta    = vzt::Vec2(inputs.deltaMousePosition);
-            m_transform->rotate(strength * inputs.deltaTime * glm::vec3(delta.y, 0.f, delta.x));
+            m_transform->rotate(strength * glm::vec3(delta.y, 0.f, delta.x));
 
             changed = true;
         }
 
         if (inputs.mouseRightPressed)
         {
-            const float strength = handleStrength(1e1f);
+            const float strength = handleStrength(1e-2f);
             const float delta    = static_cast<float>(inputs.deltaMousePosition.x);
-            m_transform->rotate(strength * -inputs.deltaTime * glm::vec3(0.f, delta, 0.f));
+            m_transform->rotate(strength * -glm::vec3(0.f, delta, 0.f));
 
             changed = true;
         }
 
         vzt::Vec3 translation{};
+        if (inputs.mouseMiddlePressed)
+        {
+            const float strength = handleStrength(2.f);
+
+            const vzt::Vec2 delta = strength * vzt::Vec2(inputs.deltaMousePosition);
+            translation += delta.y * Transform::Up;
+            translation += -delta.x * Transform::Right;
+
+            changed = true;
+        }
+
         if (inputs.isPressed(vzt::Key::W))
             translation += Transform::Front;
         if (inputs.isPressed(vzt::Key::A))
@@ -55,8 +66,8 @@ namespace lop
 
         if (translation != vzt::Vec3{})
         {
-            const float strength = handleStrength(2.f);
-            m_transform->translate(strength * inputs.deltaTime * glm::normalize(m_transform->rotation * translation));
+            const float strength = handleStrength(1e-2f);
+            m_transform->translate(strength * glm::normalize(m_transform->rotation * translation));
             changed = true;
         }
 
